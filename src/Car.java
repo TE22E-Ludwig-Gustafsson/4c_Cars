da8ec35a05996b6840e3e90227283278cb5a94da
Ludwig Gustafsson <ludwig.gustafsson@elev.ga.ntig.se>
2024-10-01 10:28:45 +0200

    uppdatering

---
+++ src/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    String color;
+    int speed = 0;
+    String name;
+    int maxSpeed;
+
+
+    public
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Working with Cars");
+
+        //Datatype , variableName = Create the object
+        //  Car          car1     = new Car();
+
+        //create a car
+        Car car1 = new Car();
+
+        //Give the attributes values
+        car1.name = "Hudson";
+        car1.color = "blue";
+        car1.maxSpeed = 200;
+
+
+        //create a second car
+        Car car2 = new Car();
+
+        //Give the attributes values
+        car2.name = "Hudson";
+        car2.color = "blue";
+        car2.maxSpeed = 200;
     }
+
 }
